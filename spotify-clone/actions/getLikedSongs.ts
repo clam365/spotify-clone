28ab2a7332465baa6219425e585e51a5cce17df3
Christopher Lam <127339027+clam365@users.noreply.github.com>
2024-06-26 13:29:07 -0400

    Liked Song Functionality

---
+++ spotify-clone/actions/getLikedSongs.ts
@@ -0,0 +1,25 @@
+import {Song} from "@/types";
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
+import {cookies} from "next/headers";
+
+
+//Backend function fetching our songs from Supabase. Most clear-cut example of fullstack implementation
+const getLikedSongs = async (): Promise<Song[]> => {
+    const supabase = createServerComponentClient({
+        cookies: cookies,
+
+    });
+
+    const {data, error} = await supabase
+        .from('songs')
+        .select('*')
+        .order('created_at', {ascending: false});
+
+        if (error) {
+            console.log(error);
+        }
+
+    return  (data as any ) || [];
+}
+
+export default getLikedSongs;

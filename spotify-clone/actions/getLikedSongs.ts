5ecf968847943f88e2026d87e140ce4377e011a0
Christopher Lam <127339027+clam365@users.noreply.github.com>
2024-06-26 14:21:42 -0400

    Liked Song Functionality Fully Completed with Page

--- spotify-clone/actions/getLikedSongs.ts
+++ spotify-clone/actions/getLikedSongs.ts
@@ -7,19 +7,33 @@ import {cookies} from "next/headers";
 const getLikedSongs = async (): Promise<Song[]> => {
     const supabase = createServerComponentClient({
         cookies: cookies,
-
     });
 
+    const {
+        data: {
+            session
+        }
+    } = await supabase.auth.getSession();
+
     const {data, error} = await supabase
-        .from('songs')
-        .select('*')
+        .from('liked_songs')
+        .select('*, songs(*)')
+        .eq('user_id', session?.user?.id)
         .order('created_at', {ascending: false});
 
         if (error) {
             console.log(error);
+            return [];
         }
 
-    return  (data as any ) || [];
+        if (!data) {
+            return [];
+        }
+
+    //Spreading relations
+    return data.map( (item) => ({
+        ...item.songs
+    }));
 }
 
 export default getLikedSongs;
